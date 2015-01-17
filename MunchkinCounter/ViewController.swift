0c0dd6da95815593d20660ce4498d988bbadf6e6
Alex Gaesser <agaesser@gmail.com>
2015-01-17 12:24:47 -0500

    Transitions added
    The back buttons are a modal segue, which apparently you’re not
    supposed to do. I have some information on a “dismiss current view”
    action, but I can’t insert it into the view controller.h file. I’m also
    having a hard time figuring out how to create a view controller file
    for the specific view.

--- MunchkinCounter/ViewController.swift
+++ MunchkinCounter/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func scoreBoardButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func faqsAndRulesButtonPressed(sender: UIButton) {
+    }
 
+    @IBAction func settingsButtonPressed(sender: UIButton) {
+    }
 }
 
