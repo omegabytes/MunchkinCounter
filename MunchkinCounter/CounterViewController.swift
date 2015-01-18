1390886ce1611816de4ac7b8e7764c387fcf721a
Alex Gaesser <agaesser@gmail.com>
2015-01-17 19:59:36 -0500

    Counter View
    started work on the variables

--- MunchkinCounter/CounterViewController.swift
+++ MunchkinCounter/CounterViewController.swift
@@ -9,11 +9,25 @@
 import UIKit
 
 class CounterViewController: UIViewController {
-
+    @IBOutlet weak var levelLabel: UILabel!
+    @IBOutlet weak var combatLabel: UILabel!
+    @IBOutlet weak var oneShotLabel: UILabel!
+    @IBOutlet weak var effectiveCombatLabel: UILabel!
+    @IBOutlet weak var netCombatResultLabel: UILabel!
+    @IBOutlet weak var monsterCombatStrengthTextField: UITextField!
+    @IBOutlet weak var combatOutcomeTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        var level = 1
+        var combat = 0
+        var oneShot = 0
+        var monsterCombatStrength = 0
+        
+        var effectiveCombat:Int = level + combat + oneShot
+        var netCombatResult:Int
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +35,27 @@ class CounterViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func levelDecrementButtonPressed(sender: UIButton) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
 
+    @IBAction func levelIncrementButtonPressed(sender: UIButton) {
+    }
+
+    @IBAction func combatDecrementButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func combatIncrementButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func oneShotDecrementButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func oneShotIncrementButtonPressed(sender: UIButton) {
+    }
+    
+//    @IBAction func editTextFieldDidEnd(sender: UITextField) {
+//        monsterCombatStrengthTextField.resignFirstResponder()
+//    }
+    
 }
